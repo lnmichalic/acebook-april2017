cc2814dcbc39a5eb8890ee8810833e8c009c11ad
Adam Snow <adamjohnsnow@icloud.com>
2017-05-30 17:29:21 +0100

    button to like post with count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530112642) do
+ActiveRecord::Schema.define(version: 20170530150347) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170530112642) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "like_id"
+    t.index ["like_id"], name: "index_posts_on_like_id"
   end
 
   create_table "users", force: :cascade do |t|
