21351be2488016a3a22d199fc23d0e2c47d61444
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-30 11:08:38 +0100

    Attempted to fix hound violations

--- spec/features/posts_have_dates_spec.rb
+++ spec/features/posts_have_dates_spec.rb
@@ -4,11 +4,10 @@ RSpec.feature 'Posts show the date and time they were posted', type: :feature do
   scenario 'Posts have date and time attatched' do
     visit '/posts'
     click_link 'New post'
-    fill_in "Message", with: 'Post Number One'
-    click_button "Submit"
-    date = Time.now.utc.strftime("%d/%m/%Y")
-    time = Time.now.utc.strftime("%H:%M")
-    puts time
+    fill_in 'Message', with: 'Post Number One'
+    click_button 'Submit'
+    date = Time.now.utc.strftime('%d/%m/%Y')
+    time = Time.now.utc.strftime('%H:%M')
     expect(page).to have_content("Post Number One Posted on #{date} at #{time}")
   end
 end
