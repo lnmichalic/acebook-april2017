a82d20ee23b9ee781641df6922a293051a76b551
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-30 16:51:04 +0100

    Adds upload image feature

---
+++ spec/features/user_posts_photos_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can post photos to the timeline', type: :feature do
+  scenario 'Uploaded images appear on the next screen' do
+    visit '/posts'
+    click_link 'New post'
+    attach_file('post_image', Rails.root + 'public/owl.jpg')
+    fill_in 'Message', with: 'Test post'
+    click_button 'Submit'
+    save_and_open_page
+    expect(page).to have_xpath("//img[contains(@src,'owl.jpg')]")
+  end
+end
