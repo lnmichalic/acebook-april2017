8061d858452376efd25f5d16d6b5cab6a88ec510
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-30 17:59:12 +0100

    url creator extracted to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def url_checker(post)
+    simple_format(post).gsub(URI.regexp, '<a href="\0">\0</a>').html_safe
+  end
+
 end
---
+++ spec/features/user_can_post_links_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'URL are converted to clickable links', type: :feature do
+  scenario 'user can click on links posted' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'http://rubyonrails.org/'
+    click_button 'Submit'
+    expect(page).to have_link('', href: 'http://rubyonrails.org/')
+  end
+end
