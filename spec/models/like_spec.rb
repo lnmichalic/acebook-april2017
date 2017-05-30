63a8f6dce2bdc346ccf9894e74da913204c0eb7e
JayWebDevCom <jaiye.s@gmail.com>
2017-05-30 13:51:00 +0100

    add passing like test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be created with user id and post id' do
+    user = User.create(email: "test@example.com", password: "secret")
+    post = Post.create(message: "post message")
+    like = Like.create(post_id: post.id, user_id: user.id)
+    expect(like.user_id).to eq user.id
+    expect(like.post_id).to eq post.id
+    expect(like.id).not_to be_nil
+  end
 end
