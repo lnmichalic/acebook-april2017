5b1db8f7abc46a1cc09fac0e2ba0dbaa50f2231f
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-30 16:25:49 +0100

    Comments cannot be empty, shorter than 2 characters or longer than 150

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe Comment, type: :model do
     expect(comment.body).to eq("This is a comment")
     expect(comment.id).not_to be_nil
   end
+  it 'Cannot be empty' do
+    user = User.create(email: "test@example.com", password: "secret")
+    post = Post.create(message: "post message")
+    comment = Comment.create(body: " ", post_id: post.id, user_id: user.id)
+    expect(comment.id).to be_nil
+  end
+
+  it 'Cannot be longer than 150 characters' do
+    user = User.create(email: "test@example.com", password: "secret")
+    post = Post.create(message: "post message")
+    comment = Comment.create(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", post_id: post.id, user_id: user.id)
+    expect(comment.id).to be_nil
+  end
+
+  it 'Cannot be shorter than 3 characters' do
+    user = User.create(email: "test@example.com", password: "secret")
+    post = Post.create(message: "post message")
+    comment = Comment.create(body: "A", post_id: post.id, user_id: user.id)
+    expect(comment.id).to be_nil
+  end
+
 end
