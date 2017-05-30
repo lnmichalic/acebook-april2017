c59de52fa7cf24fea7e6b2985f5ab09559900303
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-30 11:15:33 +0100

    Now working in BST

--- spec/features/posts_have_dates_spec.rb
+++ spec/features/posts_have_dates_spec.rb
@@ -6,8 +6,8 @@ RSpec.feature 'Posts show the date and time they were posted', type: :feature do
     click_link 'New post'
     fill_in 'Message', with: 'Post Number One'
     click_button 'Submit'
-    date = Time.now.utc.strftime('%d/%m/%Y')
-    time = Time.now.utc.strftime('%H:%M')
+    date = Time.now.strftime('%d/%m/%Y')
+    time = Time.now.strftime('%H:%M')
     expect(page).to have_content("Post Number One Posted on #{date} at #{time}")
   end
 end
