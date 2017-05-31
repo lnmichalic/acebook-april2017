80c97b9a3a1760130ad2de51d4f86a2cd3dab107
Henry Hobhouse <henryhobhouse@gmail.com>
2017-05-31 11:25:37 +0100

    updated schema

---
+++ db/migrate/20170531081508_add_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string, { limit: 150 }
+    change_column_null :users, :name, false
+    add_index :users, :name
+  end
+end
