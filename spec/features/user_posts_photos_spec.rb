5ab243c68299fc3f90f3784b8a26928cb8ad7709
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-31 11:34:40 +0100

    Test passes using sign_in helper

--- spec/features/user_posts_photos_spec.rb
+++ spec/features/user_posts_photos_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Users can post photos to the timeline', type: :feature do
   scenario 'Uploaded images appear on the next screen' do
-    visit '/posts'
+    sign_in
     click_link 'New post'
     attach_file('post_image', Rails.root + 'public/owl.jpg')
     fill_in 'Message', with: 'Test post'
