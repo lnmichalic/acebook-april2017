40952e77e7db55ec1393d049b198fd6f16aaced9
JayWebDevCom <jaiye.s@gmail.com>
2017-05-31 13:12:45 +0100

    use clearance methods git status

--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,23 @@
 
 ActiveRecord::Schema.define(version: 20170530150347) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "like_id"
+    t.bigint "like_id"
     t.index ["like_id"], name: "index_posts_on_like_id"
   end
 
@@ -43,4 +43,7 @@ ActiveRecord::Schema.define(version: 20170530150347) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "likes"
 end
--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -1,34 +1,37 @@
 require 'rails_helper'
 require "support/features/likes_helpers"
+require "support/features/clearance_helpers"
+
 
 RSpec.feature "Likes", type: :feature do
 
-  scenario "Cannot see like button unless signed in" do
+  scenario "A user cannot see like button unless signed in" do
+    sign_in
     make_post
+    sign_out
+    visit '/posts'
+    expect(page).to have_content "Post to be liked"
     expect(page).not_to have_button "Like (0)"
   end
 
-  scenario "Has like button with like count" do
-    email = "name@example.com"
-    sign_user_up(email)
+  scenario "A post has a like button with like count" do
+    sign_in
     make_post
     expect(page).to have_button "Like (0)"
   end
 
-  scenario "Can like a post" do
-    email = "name@example.com"
-    sign_user_up(email)
+  scenario "A user can like a post" do
+    sign_in
     make_post
     click_button "Like (0)"
     expect(page).to have_button("Like (1)", disabled: true)
   end
 
-  scenario "Cannot like a post twice" do
-    email = "name@example.com"
-    sign_user_up(email)
+  scenario "A user cannot like a post twice" do
+    sign_in
     make_post
     click_button "Like (0)"
-    expect(page).not_to have_button("Like (1)")
+    expect(page).not_to have_button("Like (1)", disabled: false)
   end
 
 end
