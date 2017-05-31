c98a994a8887166dbf1a45b76a80b1fb6a56809a
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-31 11:51:33 +0100

    More requested changes

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,21 +15,21 @@ RSpec.describe Comment, type: :model do
     user = User.create(email: "test@example.com", password: "secret")
     post = Post.create(message: "post message")
     comment = Comment.create(body: " ", post_id: post.id, user_id: user.id)
-    expect(comment.id).to be_nil
+    expect(comment).to_not be_valid
   end
 
   it 'Cannot be longer than 150 characters' do
     user = User.create(email: "test@example.com", password: "secret")
     post = Post.create(message: "post message")
     comment = Comment.create(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", post_id: post.id, user_id: user.id)
-    expect(comment.id).to be_nil
+    expect(comment).to_not be_valid
   end
 
   it 'Cannot be shorter than 2 characters' do
     user = User.create(email: "test@example.com", password: "secret")
     post = Post.create(message: "post message")
     comment = Comment.create(body: "A", post_id: post.id, user_id: user.id)
-    expect(comment.id).to be_nil
+    expect(comment).to_not be_valid
   end
 
 end
