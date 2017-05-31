8943ae74d27d97457cf7c549bd116b5b715ba538
Henry Hobhouse <henryhobhouse@gmail.com>
2017-05-31 11:14:48 +0100

    name migration file updated file and class name

--- db/migrate/20170531081508_add_nameto_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNametoUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :name, :string, { limit: 150 }
-    change_column_null :users, :name, false
-    add_index :users, :name
-  end
-end
