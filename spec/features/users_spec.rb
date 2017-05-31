cf76c9d7ddaf90654e83af200ef83af04dff595e
Henry Hobhouse <henryhobhouse@gmail.com>
2017-05-31 13:38:30 +0100

    refactored after rubocop suggestions

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -20,11 +20,11 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   scenario 'user can click on links posted' do
-    sign_in
-    visit '/posts'
-    click_link 'New post'
-    fill_in 'Message', with: 'http://rubyonrails.org/'
-    click_button 'Submit'
-    expect(page).to have_link('', href: 'http://rubyonrails.org/')
+   sign_in
+   visit '/posts'
+   click_link 'New post'
+   fill_in 'Message', with: 'http://rubyonrails.org/'
+   click_button 'Submit'
+   expect(page).to have_link('', href: 'http://rubyonrails.org/')
  end
 end
