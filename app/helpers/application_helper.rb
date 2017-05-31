b09477cb714878e11e219346058c5d63abe2fb0d
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-05-31 11:19:30 +0100

    Fixed post and date test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def post_presenter(post)
-    simple_format("#{url_checker(post.message)}<br>Posted on #{post.created_at.strftime("%d/%m/%Y")} at #{post.created_at.strftime("%H:%M")}")
+    simple_format("#{url_checker(post.message)} <br>Posted on #{post.created_at.strftime("%d/%m/%Y")} at #{post.created_at.strftime("%H:%M")}")
   end
 end
