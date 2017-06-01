7c235cb7b6969d8adeaacba880703c06b849927b
Adam Snow <adamjohnsnow@icloud.com>
2017-06-01 09:15:39 +0100

    added constant for avatar sizes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
+  AVATAR_SIZES = {:small => 70, :medium => 150, :large => 300}
   validates :name, presence: true
   mount_uploader :avatar, ImageUploader
 end
