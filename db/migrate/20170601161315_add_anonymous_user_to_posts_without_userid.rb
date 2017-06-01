c258e4697a5837365673655484bf151b12043301
JayWebDevCom <jaiye.s@gmail.com>
2017-06-01 18:58:21 +0100

    add bang refactor to migration file so that errors are thrown

--- db/migrate/20170601161315_add_anonymous_user_to_posts_without_userid.rb
+++ db/migrate/20170601161315_add_anonymous_user_to_posts_without_userid.rb
@@ -2,19 +2,19 @@
 class AddAnonymousUserToPostsWithoutUserid < ActiveRecord::Migration[5.1]
   def up
     password = (0...15).map { ('a'..'z').to_a[rand(25)] }.join
-    user = User.create(name: "Anonymous", email: "anon@acebook.com", password: password)
-    Post.all.each { |post|
+    user = User.create!(name: "Anonymous", email: "anon@acebook.com", password: password)
+    Post.all.each do |post|
       post.user_id ||= user.id
-      post.save
-    }
+      post.save!
+    end
   end
 
   def down
     user = User.find_by(email: "anon@acebook.com")
-    user.posts.each { |post|
+    user.posts.each do |post|
       post.user_id = nil
-      post.save(validate: false)
-    }
-    user.destroy
+      post.save!(validate: false)
+    end
+    user.destroy!
   end
 end
