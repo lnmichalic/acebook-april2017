a290ffaa57a2b3da083e7106473c774c8f39b468
Filipe Fasolin <ffasolin@gmail.com>
2017-06-01 14:45:02 +0100

    Adds separate file for footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170531150933) do
 
-ActiveRecord::Schema.define(version: 20170531141139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
