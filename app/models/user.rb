cee2efe26094c69f599640d7189e3a9145cbb4ef
Adam Snow <adamjohnsnow@icloud.com>
2017-06-01 14:35:47 +0100

    add some red

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
-  AVATAR_SIZES = { small: 70, medium: 150, large: 300 }
+  AVATAR_SIZES = { small: 50, medium: 150, large: 300 }
   validates :name, presence: true
   has_many :posts
   mount_uploader :avatar, ImageUploader
