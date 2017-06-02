c28561ffd1acc68afe67dc0e088cf9f50c4e9124
Kay Lovelace <kay@makersacademy.com>
2017-06-02 14:31:55 +0100

    [WIP] Going through with Henry

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,3 +1,22 @@
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
 function likeCount(post_id, likes) {
-  document.getElementById('count-' + post_id).innerHTML = likes + 1;
+  document.getElementById('count_' + post_id).innerHTML = likes + 1;
 };
+
+ready(function() {
+  document.querySelectorAll('.like-button').forEach(function(element) {
+    element.addEventListener('click', function(evt) {
+      var postId = evt.target.dataset.postId;
+      var countElement = document.getElementById("count_" + postId);
+      countElement.innerText = parseInt(countElement.innerText) + 1;
+      evt.preventDefault();
+    });
+  });
+});
