0dd2e08c3cbabdd89675f065109b89b7ddfd8603
isabellarner <larner.isabel@gmail.com>
2017-06-02 20:57:38 +0100

    edits on Kay's advice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,8 +32,8 @@ var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,3500);
 
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-};
+  slides[currentSlide].className = 'slide';
+    currentSlide = ( currentSlide + 1 ) % slides.length;
+      slides[currentSlide].className = 'slide showing';
+  };
 });
