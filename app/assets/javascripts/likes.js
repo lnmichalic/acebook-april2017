bafbb3b3ca0af537e4c2a842abf9d9ca4e37df31
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-02 11:17:45 +0100

    re-added files from prior branch

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function like_count(post_id, likes) {
+  document.getElementById('count_' + post_id).innerHTML = likes + 1;
+}
