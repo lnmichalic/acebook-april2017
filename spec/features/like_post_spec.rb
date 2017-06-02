5729d1fe20cb5dc3085319db4d080b7b622d9e96
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-02 11:23:06 +0100

    tests now working

--- spec/features/like_post_spec.rb
+++ spec/features/like_post_spec.rb
@@ -2,26 +2,27 @@ require 'rails_helper'
 require "support/features/likes_helpers"
 require "support/features/clearance_helpers"
 
-RSpec.feature "Likes", type: :feature do
+RSpec.feature "Like Post Functionality", type: :feature do
 
-  scenario "A post has a like button with like count" do
+  scenario "A post has a like button" do
     sign_in
     make_post
-    expect(page).to have_button "Like (0)"
+    expect(page).to have_button "Like"
   end
 
   scenario "A user can like a post" do
     sign_in
     make_post
-    click_button "Like (0)"
-    expect(page).to have_button("Like (1)", disabled: true)
+    click_button "Like"
+    visit '/'
+    expect(page).to have_content("1")
   end
 
   scenario "A user cannot like a post twice" do
     sign_in
     make_post
-    click_button "Like (0)"
-    expect(page).not_to have_button("Like (1)", disabled: false)
+    click_button "Like"
+    expect(page).not_to have_button "Like"
   end
 
 end
